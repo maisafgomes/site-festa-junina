d67da2604ff03ac8eb799d7806cb55b81cab1b9a
Maísa Fernandes Gomes <54985212+maisafgomes@users.noreply.github.com>
2025-06-15 17:55:43 -0300

    Delete site-festa-junina directory

--- site-festa-junina/frontend/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('uploadForm').addEventListener('submit', async (e) => {
-  e.preventDefault();
-  const formData = new FormData(e.target);
-  await fetch('http://localhost:5000/upload', {
-    method: 'POST',
-    body: formData
-  });
-  alert('Foto enviada!');
-});
